f7cdbc2e5c21081adca40c0e5082c6b09f47d7de
Maxime Raoust <max@nadeo.com>
2015-02-02 16:37:39 +0100

    Refactored converters, breaking changes if you used them (add missing untracked files)

---
+++ src/Manialib/Formatting/Converter/Html.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Manialib\Formatting\Converter;
+
+use Manialib\Formatting\Color;
+use Manialib\Formatting\Parser;
+
+class Html extends Parser
+{
+    protected $link = null;
+
+    protected function none($value)
+    {
+        $style = '';
+        if ($this->currentStyle->getColor()) {
+            $color = Color::stringToRgb24($this->currentStyle->getColor());
+            $style .= sprintf('color:#%s;', Color::rgb24ToString($color));
+        }
+        switch ($this->currentStyle->getWidth()) {
+        case 0:
+            $style .= 'letter-spacing:-.1em;font-size:95%;';
+            break;
+        case 2:
+            $style .= 'letter-spacing:.1em;font-size:105%;';
+            break;
+        case 1:
+        default:
+        }
+        if ($this->currentStyle->isBold()) {
+            $style .= 'font-weight:bold;';
+        }
+        if ($this->currentStyle->isItalic()) {
+            $style .= 'font-style:italic;';
+        }
+        if ($this->currentStyle->isShadowed()) {
+            $style .= 'text-shadow:1px 1px 1px rgba(0,0,0,.5);';
+        }
+        if ($this->currentStyle->isUppercase()) {
+            $style .= 'text-transform:uppercase;';
+        }
+        if ($style) {
+            $value = sprintf('<span style="%s">%s</span>', $style, $value);
+        }
+        if ($this->link) {
+            $value = sprintf('<a href="%s" style="color:inherit;">%s</a>', $this->link, $value);
+        }
+        $this->result .= $value;
+    }
+
+    protected function escapedCharacter($value)
+    {
+        $this->none($value);
+    }
+
+    protected function color()
+    {
+    }
+
+    protected function shadowed()
+    {
+    }
+
+    protected function bold()
+    {
+    }
+
+    protected function italic()
+    {
+    }
+
+    protected function wide()
+    {
+    }
+
+    protected function narrow()
+    {
+    }
+
+    protected function medium()
+    {
+    }
+
+    protected function upperCase()
+    {
+    }
+
+    protected function resetAll()
+    {
+        $this->closeLink();
+    }
+
+    protected function pushStyle()
+    {
+    }
+
+    protected function popStyle()
+    {
+    }
+
+    protected function openExternalLink($link)
+    {
+        $this->link = $link;
+    }
+
+    protected function closeExternalLink()
+    {
+        $this->closeLink();
+    }
+
+    protected function openInternalLink($link)
+    {
+        $this->link = $this->secureInternalLink($link);
+    }
+
+    protected function closeInternalLink()
+    {
+        $this->closeLink();
+    }
+
+    protected function closeLink()
+    {
+        $this->link = null;
+    }
+
+    protected function secureInternalLink($link)
+    {
+        $protocol = 'maniaplanet://';
+        if (substr($link, 0, strlen($protocol)) != $protocol) {
+            $link = sprintf('maniaplanet:///:%s', $link);
+        }
+
+        return $link;
+    }
+}
---
+++ src/Manialib/Formatting/Parser.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace Manialib\Formatting;
+
+abstract class Parser implements ConverterInterface
+{
+    /**
+     * @var Lexer
+     */
+    protected $lexer;
+
+    /**
+     * @var array
+     */
+    protected $stylesStack = array();
+
+    /**
+     * @var Style
+     */
+    protected $currentStyle;
+
+    /**
+     * @var bool
+     */
+    protected $isInLink = false;
+
+    /**
+     * @var string
+     */
+    protected $result = '';
+
+    /**
+     * @var array
+     */
+    protected $lookaheadToSkip = array();
+
+    public function __construct()
+    {
+        $this->lexer = new Lexer();
+    }
+
+    public function setInput(StringInterface $string)
+    {
+        $this->result = null;
+        $this->lexer->setInput((string)$string);
+        $this->currentStyle = new Style();
+        return $this;
+    }
+
+    public function getOutput()
+    {
+        if (!$this->result) {
+            $this->parseString();
+        }
+
+        return $this->result;
+    }
+
+    protected function parseString()
+    {
+        while ($this->lexer->moveNext()) {
+            if (in_array($this->lexer->lookahead, $this->lookaheadToSkip)) {
+                continue;
+            }
+
+            $value = $this->lexer->lookahead['value'];
+            switch ($this->lexer->lookahead['type']) {
+                case Lexer::T_NONE:
+                    $this->none($value);
+                    break;
+                case Lexer::T_ESCAPED_CHAR:
+                    $this->escapedCharacter($value);
+                    break;
+                case Lexer::T_COLOR:
+                    $color = preg_replace('/([^0-9a-f])/iu', '0', $value);
+                    $this->currentStyle->setColor($color);
+                    $this->color();
+                    break;
+                case Lexer::T_NO_COLOR:
+                    if ($this->stylesStack) {
+                        $color = $this->stylesStack[count($this->stylesStack) - 1]->getColor();
+                    } else {
+                        $color = null;
+                    }
+                    $this->currentStyle->setColor($color);
+                    $this->color();
+                    break;
+                case Lexer::T_SHADOWED:
+                    $this->currentStyle->setShadowed(!$this->currentStyle->isShadowed());
+                    $this->shadowed();
+                    break;
+                case Lexer::T_BOLD:
+                    $this->currentStyle->setBold(!$this->currentStyle->isBold());
+                    $this->bold();
+                    break;
+                case Lexer::T_ITALIC:
+                    $this->currentStyle->setItalic(!$this->currentStyle->isItalic());
+                    $this->italic();
+                    break;
+                case Lexer::T_WIDE:
+                    $this->currentStyle->setWidth(2);
+                    $this->wide();
+                    break;
+                case Lexer::T_NARROW:
+                    $this->currentStyle->setWidth(0);
+                    $this->narrow();
+                    break;
+                case Lexer::T_MEDIUM:
+                    $this->currentStyle->setWidth(1);
+                    $this->medium();
+                    break;
+                case Lexer::T_UPPERCASE:
+                    $this->currentStyle->setUppercase(!$this->currentStyle->isUppercase());
+                    $this->upperCase();
+                    break;
+                case Lexer::T_RESET_ALL:
+                    if ($this->stylesStack) {
+                        $style = $this->stylesStack[count($this->stylesStack) - 1];
+                    } else {
+                        $style = new Style();
+                    }
+                    $this->currentStyle = $style;
+                    $this->isInLink = false;
+                    $this->resetAll();
+                    break;
+                case Lexer::T_PUSH:
+                    array_push($this->stylesStack, $this->currentStyle);
+                    $this->pushStyle();
+                    break;
+                case Lexer::T_POP:
+                    $this->currentStyle = array_pop($this->stylesStack);
+                    $this->popStyle();
+                    break;
+                case Lexer::T_EXTERNAL_LINK:
+                    $this->isInLink = !$this->isInLink;
+                    if ($this->isInLink) {
+                        $link = $this->getLink();
+                        $this->openExternalLink($link);
+                    } else {
+                        $this->closeExternalLink();
+                    }
+                    break;
+                case Lexer::T_INTERNAL_LINK:
+                    $this->isInLink = !$this->isInLink;
+                    if ($this->isInLink) {
+                        $link = $this->getLink();
+                        $this->openInternalLink($link);
+                    } else {
+                        $this->closeInternalLink();
+                    }
+                    break;
+                case Lexer::T_UNKNOWN_MARKUP:
+                    // We do nothing with unknown markup for the moment
+                    break;
+            }
+        }
+    }
+
+    private function getLink()
+    {
+        $link = '';
+        if (substr($this->lexer->lookahead['value'], 1, 1) == '['
+            && substr($this->lexer->lookahead['value'], -1, 1) == ']'
+        ) {
+            //We are looking for a link like $h[xxx]yyy$h
+            $this->lexer->moveNext();
+            $matches = array();
+            if ($this->lexer->lookahead
+                && preg_match('/^\[([^\]]*)\]$/iu', $this->lexer->lookahead['value'], $matches)
+            ) {
+                $link = $matches[1];
+            }
+        } else {
+            //We are looking for a link like $hxxxx$h
+            $endLinkTokens = array(
+                Lexer::T_EXTERNAL_LINK,
+                Lexer::T_INTERNAL_LINK,
+                Lexer::T_RESET_ALL,
+            );
+            do {
+                $nextLookahead = $this->lexer->peek();
+                if ($nextLookahead
+                    && ($nextLookahead['type'] == Lexer::T_NONE || $nextLookahead['type'] == Lexer::T_ESCAPED_CHAR)
+                ) {
+                    $link .= $nextLookahead['value'];
+                    if (substr($link, 0, 1) == '[') {
+                        //It means that there is no closing square bracket $l[noclosingsquarebracket
+                        array_push($this->lookaheadToSkip, $nextLookahead);
+                    }
+                }
+            } while ($nextLookahead !== null && !in_array($nextLookahead['type'], $endLinkTokens));
+
+            if (substr($link, 0, 1) == '[') {
+                //It means that there is no closing square bracket $l[noclosingsquarebracket
+                $link = '';
+            }
+        }
+
+        return $link;
+    }
+
+    abstract protected function none($value);
+
+    abstract protected function escapedCharacter($value);
+
+    abstract protected function color();
+
+    abstract protected function shadowed();
+
+    abstract protected function bold();
+
+    abstract protected function italic();
+
+    abstract protected function wide();
+
+    abstract protected function narrow();
+
+    abstract protected function medium();
+
+    abstract protected function upperCase();
+
+    abstract protected function resetAll();
+
+    abstract protected function pushStyle();
+
+    abstract protected function popStyle();
+
+    abstract protected function openExternalLink($link);
+
+    abstract protected function closeExternalLink();
+
+    abstract protected function openInternalLink($link);
+
+    abstract protected function closeInternalLink();
+}
