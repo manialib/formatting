b6e1a69600f74c1e9c69cfa084f6d83e088c3957
Baptiste Lafontaine <baptiste@nadeo.com>
2016-06-22 09:18:25 +0200

    Add a test about parsing $< and $> (that was failing before fix)

--- tests/HtmlTest.php
+++ tests/HtmlTest.php
@@ -13,7 +13,14 @@ class HtmlTest extends PHPUnit_Framework_TestCase
                 '$cfeg$fff๐u1 $666ツ',
                 '<span style="color:#cfe;">g</span><span style="color:#fff;">๐u1 </span><span style="color:#666;">ツ</span>'
             ],
-            ['a$>b', 'ab']
+            [
+                'a$>b',
+                'ab'
+            ],
+            [
+                'foo$<$f20foo$>bar',
+                'foo<span style="color:#f20;">foo</span>bar'
+            ]
         ];
     }
 
